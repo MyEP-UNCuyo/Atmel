e9edb81f1d24209406bacb21a210c2e6d32344c0
MyEP-UNCuyo <MyEP-UNCuyo@users.noreply.github.com>
2022-03-19 20:31:42 -0300

    Update main.asm

--- GPIO/Assembler/Ej03_Blink_Delay/main.asm
+++ GPIO/Assembler/Ej03_Blink_Delay/main.asm
@@ -14,25 +14,25 @@ start:
     ldi		r16, 0b00100000
 	out		DDRB, r16
 loop:	
-	IN		r17, PORTB		; Lee puerto B, guarda en r15							1
-	EOR		r17, r16			; or exclusiva r15 y r16, guarda en r15				1
-	OUT		PORTB, r17		; escribe r15 en puerto B (no modifica los dem·s bits	1
-	CALL	delay			; 														4
-    rjmp	loop			; salto relativo, 2 ciclos.								2
+	IN		r17, PORTB		; Lee puerto B, guarda en r15				1
+	EOR		r17, r16			; or exclusiva r15 y r16, guarda en r15		1
+	OUT		PORTB, r17		; escribe r15 en puerto B (no modifica los dem√°s bits	1
+	CALL	delay			; 								4
+    rjmp	loop			; salto relativo, 2 ciclos.					2
 
 delay:	
-	ldi		r17, T1		;														1
+	ldi		r17, T1		;1
 dly1:
-	ldi		r18, T2		;														1 x							T1
+	ldi		r18, T2		;1 x T1
 dly2:	
-	ldi		r19, T3		;														1 x		 T2 x T1
+	ldi		r19, T3		;1 x		 T2 x T1
 dly3:
-	dec		r19			;														1 x T3 x T2 x T1
-	brne	dly3		;														2 x T3 x T2 x T1 + 1 x T2 x T1
-	dec		r18			;														1 x      T2 x T1 
-	brne	dly2		;														2 x      T2 x T1 + 1 x      T1
-	dec		r17			;														1 x							T1
-	brne	dly1		;														2 x							T1 + 1
-	ret					;														4
-						; 5 x T1 + 5 x T2 x T1 + 3 x T3 x T2 x T1
-						; 1000 + 250000 + 150000 x T3
+	dec		r19		;1 x T3 x T2 x T1
+	brne	dly3			;2 x T3 x T2 x T1 + 1 x T2 x T1
+	dec		r18		;1 x      T2 x T1 
+	brne	dly2			;2 x      T2 x T1 + 1 x      T1
+	dec		r17		;1 x T1
+	brne	dly1			;2 x T1 + 1
+	ret				;4
+					; 5 x T1 + 5 x T2 x T1 + 3 x T3 x T2 x T1
+					; 1000 + 250000 + 150000 x T3
